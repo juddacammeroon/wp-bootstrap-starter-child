e613bfdfe1d4b779a4b0813fd6742d4291241926
Maju Comendador <maju.comendador@webee.com.mt>
2018-08-15 19:52:09 +0800

    enqueued slick and customs scripts

--- functions.php
+++ functions.php
@@ -7,7 +7,11 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'wp-bootstrap-starter-bootstrap-css' ) );
+    	wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'wp-bootstrap-starter-bootstrap-css' ) );
+        wp_enqueue_style( 'slick-css', get_stylesheet_directory_uri() . '/slick/slick.css', array( ) );
+        wp_enqueue_style( 'slick-theme-css', get_stylesheet_directory_uri() . '/slick/slick-theme.css', array( ) );
+        wp_enqueue_script( 'slick-js', get_stylesheet_directory_uri() . '/slick/slick.min.js', array('jquery') );
+        wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/script.js', array('jquery') );
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
